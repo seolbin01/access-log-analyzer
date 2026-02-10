54d77eee6fb593040f34f91e651af3af1664143c
seolbin01 <se01bin@suwon.ac.kr>
2026-02-10 11:22:04 +0900

    chore: 프로젝트 초기 설정
    close #1

---
+++ src/main/java/benny/accessloganalyzer/AccessLogAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package benny.accessloganalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessLogAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessLogAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/benny/accessloganalyzer/AccessLogAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package benny.accessloganalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessLogAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
